495d216347bfb40ed667cb8faf18771dc28e4aec
leofitlab <lucilayalejoalx@hotmail.com>
2025-05-26 07:34:24 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import GymMemberships from './pages/GymMemberships';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfServices from './pages/TermOfService';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gyms" element={<GymMemberships />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+         <Route path="/termOfServices" element={<TermOfService />} />
+        <Route path="/contact" element={<Contact />} />
+        
       </Routes>
     </Layout>
   );
